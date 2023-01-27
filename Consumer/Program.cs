c22457eca5f5ee04ad91c11b9e0092994641cb60
cinxdy <cinxdys2@gmail.com>
2023-01-27 16:50:40 +0900

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        
+    }
+
+
+    
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Producer producer = new Producer();
+            producer.run();
+        }
+
+        // Socket 
+
+        class Producer
+        {
+            public void run()
+            {
+
+                Random rand = new Random();
+                int item = default;
+
+                using (var sender = new RequestSocket("@tcp://*:5557"))
+                using (var controller = new SubscriberSocket())
+                {
+                    // Connect
+                    //sender.Connect("tcp://127.0.0.1:5557");
+                    controller.Connect("tcp://127.0.0.1:5558");
+                    controller.Subscribe("wake producer");
+
+                    Console.WriteLine("Producer Connecting...");
+
+                    // Wait Publish (Sleep)
+                    Console.WriteLine("Waiting the signal...");
+
+                    //string messageTopicReceived = ;
+                    //string messageReceived = controller.ReceiveFrameString();
+                    //Console.WriteLine(messageReceived);
+
+                    // if signaled
+                    while (controller.ReceiveFrameString() != "wake producer")
+                        ;
+
+                    // produce one
+                    item = rand.Next(1, 1000);
+                    Console.WriteLine($"\t{item} was produced\n");
+
+                    // send
+                    // if okay : take a break
+                    // if not okay : retry
+                    while (true)
+                    {
+                        sender.SendFrame(item.ToString());
+                        if (sender.ReceiveSignal()) break;
+                        Task.Delay(1000).Wait();
+                    }
+
+                    int sleep = rand.Next(1, 5) * 1000;
+                    Task.Delay(sleep).Wait();
+                }
+                
+            }
+        }
+    }
+}
